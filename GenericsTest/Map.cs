46095d24a4f03d49d3a2a7fe09651709853b5dda
xeof-landmark <mauronobs@gmail.com>
2022-09-06 17:01:48 -0300

    Code Cleaned Code is cleaned with the solution that I could find to avoid repeating methods.

--- GenericsTest/Map.cs
+++ GenericsTest/Map.cs
@@ -1,11 +1,24 @@
 ﻿using AutoFixture;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GenericsTest
 {
     public class Map
     {
         static Fixture fixture = new Fixture();
+
+        public List<IUnit> MapList(List<IComparable<IUnit>> unitsComparables)
+        {
+            var mapUnitComparables = unitsComparables.Where(x => x is Comparable<IUnit>).Select(s => (Comparable<IUnit>)s).ToList();
+            var mapUnitNonComparables = unitsComparables.Where(x => x is NonComparable<IUnit>).Select(s => (NonComparable<IUnit>)s).ToList();
+            var result = new List<IUnit>();
+
+            result.AddRange(MapList(mapUnitComparables));
+            result.AddRange(MapList(mapUnitNonComparables));
+
+            return result;
+        }
 
         public List<IUnit> MapList(List<Comparable<IUnit>> compareOnes)
         {
